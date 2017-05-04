c8a60bf20cd5371de2ed253621f6c151d9fe9054
Brandon <idhere703@gmail.com>
2017-05-04 17:31:20 -0400

    Added model

--- gotToEat/models.py
+++ gotToEat/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Posts(models.Model):
+    title = models.TextField(max_length=100)
+    subtitle = models.TextField(max_length=200)
+    postedBy = models.TextField(max_length=200)
+    imgClass = models.TextField(max_length=100)
+    content = models.TextField()
+    postedAt = models.DateTimeField('date published')
