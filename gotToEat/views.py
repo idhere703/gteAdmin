81bedc0e4760e64d7c78b1901b5c0bbdc60f1b3d
Brandon <idhere703@gmail.com>
2017-05-09 20:06:07 -0400

    Added urls and views for API.

--- gotToEat/views.py
+++ gotToEat/views.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.http import HttpResponse
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework import viewsets
+
 
 
 def index(request):
     return HttpResponse("You're at the index.")
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Post.objects.all().order_by('-postedAt')
+    serializer_class = PostSerializer
--- gteAdmin/urls.py
+++ gteAdmin/urls.py
@@ -14,9 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from rest_framework import routers
 from django.contrib import admin
+from .gotToEat import views
 
+
+router = routers.DefaultRouter()
+router.register(r'posts', views.PostViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^gotToEat/', include('gotToEat.urls')),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
