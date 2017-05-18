beb4665fe5f09ac58df5978fe82b17b42a680c3b
Brandon <idhere703@gmail.com>
2017-05-17 21:27:33 -0400

    Added token based auth.

--- gteAdmin/urls.py
+++ gteAdmin/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-from rest_framework import routers
+from rest_framework import routers, views
 from django.contrib import admin
 from gotToEat import views
 
@@ -28,5 +28,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^gotToEat/', include('gotToEat.urls')),
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-token-auth/', views.obtain_auth_token)
 ]
