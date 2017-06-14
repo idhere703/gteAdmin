1f86d6667cf504fff4ff88fa550b72e96f591222
Brandon <idhere703@gmail.com>
2017-06-13 21:38:29 -0400

    Added refresh token route. That was easier than expected.

--- gteAdmin/urls.py
+++ gteAdmin/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from rest_framework import routers
 from django.contrib import admin
 from gotToEat import views
-from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import obtain_jwt_token,refresh_jwt_token
 
 
 router = routers.DefaultRouter()
@@ -30,4 +30,5 @@ urlpatterns = [
     url(r'^gotToEat/', include('gotToEat.urls')),
     url(r'^', include(router.urls)),
     url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api-token-refresh/', refresh_jwt_token),
 ]
